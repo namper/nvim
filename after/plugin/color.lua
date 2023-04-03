c27a832cd0e0fc84159271074e789d8abc8a03d7
Misho <nnamper@gmail.com>
2023-04-04 00:38:22 +0400

    Transparent false tokyonight

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,17 +1,19 @@
 require("tokyonight").setup({
   -- use the night style
   style = "night",
-  transparent=true,
+  transparent=false,
+  terminal_colors=false,
   -- disable italic for functions
   styles = {
     functions = "NONE" 
   },
   sidebars = { "qf", "vista_kind", "terminal", "packer" },
+  day_brightness = 0.3, -- Adjusts the brig
   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
   on_colors = function(colors)
     colors.hint = colors.green
-    colors.error = "#a83232"
+    colors.error = "#ba3030"
   end
 })
 
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight-night]]
