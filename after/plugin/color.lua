a3f911738233356a4b321d4a5c4d633448838896
Misho <nnamper@gmail.com>
2023-08-24 00:29:56 +0400

    Improved color scheme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,32 +1,24 @@
-require("tokyonight").setup({
-  -- use the night style
-  style = "night",
-  transparent=false,
-  terminal_colors=false,
-  -- disable italic for functions
-  styles = {
-    functions = "NONE" 
-  },
-  sidebars = { "qf", "vista_kind", "terminal", "packer" },
-  day_brightness = 0.3, -- Adjusts the brig
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  on_colors = function(colors)
-    colors.hint = colors.green
-    colors.error = "#ba3030"
-  end
-})
-
+-- Basing building of colors from noribuddy-kiwi
 require("noirbuddy").setup({
     preset = 'kiwi',
 })
 vim.api.nvim_set_hl(0, "CursorLineNr", {  fg='#ffa500',})
+vim.api.nvim_set_hl(0, "LineNrAbove", {  fg='#87847c',})
+vim.api.nvim_set_hl(0, "LineNrBelow", {  fg='#87847c',})
+vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg='#87847c', bg='#161a19'})
+
+vim.api.nvim_set_hl(0, "@string", { fg='#9ece6a'})
+vim.api.nvim_set_hl(0, "@boolean", { fg='orange'})
+vim.api.nvim_set_hl(0, "@constant", { fg='orange'})
+vim.api.nvim_set_hl(0, "@type.builtin", { fg='#229986'})
+vim.api.nvim_set_hl(0, "@number", { fg='#6897bb'})
+
+
+-- Indent line highlights
 vim.api.nvim_set_hl(0, "IndentBlanklineSpaceCharBlankline", {  fg='#ffa500', bg='#161a19' })
 vim.api.nvim_set_hl(0, "IndentBlanklineChar", {  fg='#323232', bg='#161a19' })
 vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", {  fg='#ffa500', bg='#161a19' })
 vim.api.nvim_set_hl(0, "IndentBlanklineContextStart", {  fg='#ffa500', bg='#161a19' })
 vim.api.nvim_set_hl(0, "IndentBlanklineSpaceChar", {  fg='#ffa500', bg='#161a19' })
-
-
--- setup must be called before loading
--- vim.cmd [[ "background = light"  ]]
--- vim.cmd.colorscheme "miami-nights"
+-- Spell
+vim.api.nvim_set_hl(0, "SpellBad", {  fg='red', })
