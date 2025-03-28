0c0daf1edbb61abd659ef8b70f88125a8fd71044
Misho <nnamper@gmail.com>
2025-03-28 23:49:07 +0400

    typos

--- after/plugin/dadbod.lua
+++ after/plugin/dadbod.lua
@@ -1,2 +1,7 @@
 vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
 vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_table_helpers = {
+  mysql = {
+     QA = 'SELECT * FROM client WHERE email like "%@growth-x.com" and client_type = "testing"',
+  }
+}
---
+++ db_ui/dev/dev.sql
@@ -0,0 +1,5 @@
+-- select * from receiver where receiver_id = 8599478;
+
+
+select campaign_id from user_receiver where receiver_id = 8599478 and user_id= 3393;
+select campaign_type, campaign_id from campaign where campaign_id = 27683;
